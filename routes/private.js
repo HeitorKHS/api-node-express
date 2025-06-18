84750b4acab1b4092fa836369b44dd00d26c944d
unknown <heitorkhs@gmail.com>
2025-06-18 20:31:33 -0300

    Create a backend with node, express, prisma and jwt

---
+++ routes/private.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const router = express.Router()
+const prisma = new PrismaClient()
+
+router.get('/Listar-usuarios', async (req, res) => {
+    
+    try{
+
+        const users = await prisma.user.findMany({
+            omit: { password: true}
+        })
+
+        res.status(200).json({ message: 'Usuários listados com sucesso', users })
+    }catch (error) {
+        res.status(500).json({ message: 'Falha no Servidor' })
+    }
+
+})
+
+export default router
